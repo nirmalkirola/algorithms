b7e14a7dc7b49ef853597985e5d1bb9222bcad8e
Nirmal Kirola <nirmalkirola@gmail.com>
2020-07-24 20:12:14 +0530

    Add files via upload

---
+++ sort/bubble_sort.py
@@ -0,0 +1,12 @@
+'''
+ Worst-case performance: O(N^2)
+'''
+
+def bubbleSort(nums):
+    for i in range(len(nums)-1,-1,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j]=nums[j+1]
+                nums[j+1] = temp
+    return nums
