be374a3e17f36e7eab1fb2d6b3049ecc3ea7a9b3
Nirmal Kirola <nirmalkirola@gmail.com>
2020-07-24 23:23:58 +0530

    Add files via upload

---
+++ sort/selection_sort.py
@@ -0,0 +1,18 @@
+'''
+Time Complexity: O(n2) as there are two nested loops
+
+'''
+
+
+def selectionSort(nums):
+    for i in range(len(nums)-1):
+        minpos = i
+        for j in range(i,len(nums)):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+    
+    return nums
